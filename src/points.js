a5096ce7fb2aa21516655adb20d7a220648ee5dc
recker112 <joseortiz112001@gmail.com>
2021-05-02 10:32:35 +0000

    FIX: Archivos innecesarios eliminados

--- src/points.js
+++
@@ -1,5 +0,0 @@
-bot.command('chavez', ctx => {
-	ctx.replyWithMarkdown(`*Jugador*: ${ctx.from.first_name} ${ctx.from.last_name}
-*EXP*: ${puntos[ctx.from.username] || 0} pts
-*Nivel*: ${puntos[ctx.from.username] || 0}`);
-});
